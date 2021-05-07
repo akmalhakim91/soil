81799b0f556c8f76a6b82c2988f2dfff4768846d
akmalhakim <akmalhakim@lcsb.com.my>
2021-05-07 11:11:20 +0800

    repair update delete

--- src/main/java/my/cwm/soil/employeeinfo/EmployeeController.java
+++ src/main/java/my/cwm/soil/employeeinfo/EmployeeController.java
@@ -13,7 +13,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,14 +49,14 @@ public class EmployeeController {
 
     @Transactional
     @PostMapping("")
-    public Employee save(Employee employee){
+    public Employee save(@RequestBody Employee employee){
         return this.svc.save(employee);
     }
 
 
     @Transactional
     @DeleteMapping("/{id}")
-    public void delete(Long id){
+    public void delete(@PathVariable  Long id){
         this.svc.delete(id);
     }
 
